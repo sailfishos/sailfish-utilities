3d549e031311f933ed9b1cc8deda0b8d7af2f158
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2014-09-01 10:56:14 +0300

    [restart-network] restart also wpa_supplicant
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- tools/restart_network.sh
+++ tools/restart_network.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-if systemctl stop connman.service; then
-    systemctl restart wlan-module-load.service
-    systemctl start connman.service
-else
-    echo "Can't stop connman"
-    exit 1
-fi
+
+function warning {
+    echo "`basename $0`: Warning: ${@:1}" 1>&2;
+}
+
+function service_do {
+    if systemctl $1 $2; then
+        return 0;
+    else
+        warning "Status $?" "on $1 $2" ${@:3}
+        return 1;
+    fi
+}
+
+service_do stop connman.service
+service_do stop wpa_supplicant.service
+service_do restart wlan-module-load.service
+service_do start wpa_supplicant.service
+service_do start connman.service
